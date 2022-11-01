1453cf5180e6470f610fb733d040770ad0862914
NathaliaMiriam <nathalia.miriam@hotmail.com>
2022-11-01 20:30:21 -0300

    starts db connection

--- src/server.js
+++ src/server.js
@@ -12,5 +12,6 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use("/api", routes);
 
 server.listen(process.env.PORT, () => {
-  console.log("Servidor rodando em: http://localhost:${process.env.PORT}");
+  console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+  //console.log("servidor subiu");
 });
