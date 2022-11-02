bd9196f5d68b1c8464a88339d91bf2d3460a2f4e
NathaliaMiriam <nathalia.miriam@hotmail.com>
2022-11-02 17:56:36 -0300

    fixs server to db connection

--- src/server.js
+++ src/server.js
@@ -13,5 +13,4 @@ server.use("/api", routes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
-  //console.log("servidor subiu");
 });
